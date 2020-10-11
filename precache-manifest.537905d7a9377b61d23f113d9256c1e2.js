3347f793b5e87f4b05a14b1af8cf3e07b01958a5
qudratgulamov <qudratgulamov6465@gmail.com>
2020-10-11 15:01:57 +0500

    Updates

---
+++ precache-manifest.537905d7a9377b61d23f113d9256c1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba187c0ace3919fa7e5cd2ec3094c9e",
+    "url": "/food-app-react/index.html"
+  },
+  {
+    "revision": "93a2abc1179afc1ce73f",
+    "url": "/food-app-react/static/css/main.66ff9b69.chunk.css"
+  },
+  {
+    "revision": "694fec99255104d5fb2b",
+    "url": "/food-app-react/static/js/2.a4a1ca50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-app-react/static/js/2.a4a1ca50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a2abc1179afc1ce73f",
+    "url": "/food-app-react/static/js/main.bb4a7bda.chunk.js"
+  },
+  {
+    "revision": "84c31f84611dd9ac6553",
+    "url": "/food-app-react/static/js/runtime-main.ccadb20c.js"
+  }
+]);
